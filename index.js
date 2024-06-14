6c960066d6eca68bdfc1c908fae8e23b2bf05861
Miguelangel Weill <miguelangel.weill@gmail.com>
2024-06-14 13:20:32 -0400

    This is the rendering of the sheet

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 let XLSX = require("xlsx");
-let workbook = XLSX.readFile("")
+let workbook = XLSX.readFile("./sheets/July_Beverage_Inventory_2024_Luis.xlsx");
+
+let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+for (let index = 2; index < 8; index++) {
+  const type = worksheet[`A${index}`];
+  const country = worksheet[`B${index}`];
+  const varietal = worksheet[`C${index}`];
+  const producer = worksheet[`D${index}`];
+  const vintage = worksheet[`E${index}`];
+  const distributor = worksheet[`F${index}`];
+  const reported_by = worksheet[`G${index}`];
+  const count = worksheet[`H${index}`];
+  let c = countt;
+  console.log(c);
+}
