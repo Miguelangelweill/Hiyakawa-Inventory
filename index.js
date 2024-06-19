e765b21b447e4fb251e9fdc53a1393b896c3e8a5
Miguelangel Weill <miguelangel.weill@gmail.com>
2024-06-19 13:41:20 -0400

    organized the object that is returned

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 let XLSX = require("xlsx");
+let fs = require("fs");
 let workbook = XLSX.readFile("./sheets/July_Beverage_Inventory_2024_Luis.xlsx");
 
 let worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
-function getCounts(){for (let i = 2; i < 8; i++) {
+function getCounts(sheets) {
+ let id = 0;
+  for (let i = 2; i < 500; i++) {
+    const type = sheets[`A${i}`];
+    const country = sheets[`B${i}`];
+    const varietal = sheets[`C${i}`];
+    const producer = sheets[`D${i}`];
+    const vintage = sheets[`E${i}`];
+    const distributor = sheets[`F${i}`];
+    const reported_by = sheets[`G${i}`];
+    const count = sheets[`H${i}`];
+    let { t, v, w } = count ?? { t: 0, v: 0, w: 0 };
+    id++
 
-  const type = worksheet[`A${i}`];
-  const country = worksheet[`B${i}`];
-  const varietal = worksheet[`C${i}`];
-  const producer = worksheet[`D${i}`];
-  const vintage = worksheet[`E${i}`];
-  const distributor = worksheet[`F${i}`];
-  const reported_by = worksheet[`G${i}`];
-  const count = worksheet[`H${i}`];
-  let {t,v,w} = count ?? {t:0,v:0,w:0};
-  console.log({
-    t,w,v,
-    producer,
-    varietal,
-    vintage,
-    type
-  });
-}};
-getCounts();
+    console.log(producer.v, varietal.v,type.v, v, "this is the id,",id );
+  }
+}
+getCounts(worksheet);
